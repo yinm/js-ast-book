4bc7af857e91763804f11c2d2b411ed41a3629b0
yinm <yinm.cminor9@gmail.com>
2019-08-03 12:31:25 +0900

    Create DI plugin

---
+++ chapter3/replace-variable-init.js
@@ -0,0 +1,22 @@
+const {transform} = require('@babel/core')
+const {parseExpression} = require('babylon')
+
+const source = 'const hoge = require("hoge")'
+
+const targetId = 'hoge'
+const replaceCode = 'require("dummy-hoge")'
+
+const plugin = ({types: t, template}) => {
+  return {
+    visitor: {
+      VariableDeclarator: (nodePath, state) => {
+        if (t.isIdentifier(nodePath.node.id) && nodePath.node.id.name === targetId) {
+          const newAst = parseExpression(replaceCode)
+          nodePath.get('init').replaceWith(newAst)
+        }
+      }
+    }
+  }
+}
+
+console.log(transform(source, {plugins: [plugin]}).code)
